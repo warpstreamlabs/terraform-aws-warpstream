9d3c7c59eb5b92e840c8d99029b87c2cb8dc445e
Jon Morehouse <morehousej09@gmail.com>
2024-04-12 19:54:17 -0600

    fix: decode json

--- service.tf
+++ service.tf
@@ -19,14 +19,14 @@ module "service" {
     operating_system_family = "LINUX"
   }
   container_definitions = {
-    "agent" = templatefile("${path.module}/container-definitions/agent.json", {
+    "agent" = jsondecode(templatefile("${path.module}/container-definitions/agent.json", {
       image      = "public.ecr.aws/warpstream-labs/warpstream_agent:${var.agent_version}",
       bucket_url = "s3://${var.bucket_name}?region=${local.bucket_region}",
       region = local.bucket_region,
       api_key    = var.api_key,
       vc_id      = var.virtual_cluster,
       warpstream_region = var.warpstream_region,
-    })
+    }))
   }
 
   iam_role_name = var.agent_role_name
